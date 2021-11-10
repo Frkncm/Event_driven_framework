17d9a77b770c26909f682fdef5789d6af79a3d98
Furkan <furkancam_38@hotmail.com>
2021-11-10 21:51:38 +0000

    updates

--- main.cpp
+++ main.cpp
@@ -11,3 +11,37 @@ int main(void)
 
     return Event_driven::esf::run();
 }
+
+// Overrided onIdle function provided by framework
+namespace Event_driven
+{
+    void esf::onIdle(void)
+    {
+        // implement when there is no event for AOs
+        char ch;
+        Event evt;
+        std::cin >> ch;
+
+        switch (ch)
+        {
+        case 'l':
+            POST(AO_myObj, LED_BLINK);
+            break;
+        case 't':
+            POST(AO_myObj, TEST_EVENT);
+            break;
+        case 'e':
+            POST(AO_myObj, TEST_TRAN);
+            break;
+        case 's':
+            POST(AO_myObj, TRIG_SUPER);
+            break;
+        case 'd':
+            POST(AO_myObj_1, TRIG_SUPER);
+            break;
+
+        default:
+            break;
+        }
+    }
+}
